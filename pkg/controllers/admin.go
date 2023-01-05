41a7aba3d44b78375d726d57a0d43b3dc0a94a1b
Nikhilnarayanan623 <Nikhilnarayan623@gmail.com>
2023-01-05 14:26:33 +0530

    admin login validatin and database check is completed

--- pkg/controllers/admin.go
+++ pkg/controllers/admin.go
@@ -3,26 +3,45 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"webApp/pkg/helper"
 
 	"github.com/gin-gonic/gin"
 )
 
+var LoginMessage interface{}
+
 func Login(ctx *gin.Context) {
 	fmt.Println("At Admin Login")
-	ctx.HTML(200, "adminLogin.html", nil)
+	ctx.HTML(200, "adminLogin.html", LoginMessage)
 }
 
 func Submit(ctx *gin.Context) {
 	fmt.Println("At Admin Submit")
 
-	//valid
+	validRes, ok := helper.ValidateAdmin(struct {
+		Email    string `validate:"required,email"`
+		Password string `validate:"required"`
+	}{
+		Email:    ctx.Request.PostFormValue("email"),
+		Password: ctx.Request.PostFormValue("password"),
+	})
+
+	if !ok {
+		LoginMessage = validRes
+		Login(ctx)
+	}
+
+	adminDetails = validRes
+	//valid admin
 	Home(ctx)
 }
+
+var adminDetails interface{}
 
 func Home(ctx *gin.Context) {
 	fmt.Println("At Admin Home")
 
-	ctx.HTML(200, "adminHome.html", nil)
+	ctx.HTML(200, "adminHome.html", adminDetails)
 }
 
 func Logout(ctx *gin.Context) {
--- pkg/helper/adminHelper.go
+++ pkg/helper/adminHelper.go
@@ -1,14 +1,62 @@
 package helper
 
 import (
+	"fmt"
 	"os"
 	"webApp/pkg/db"
 	"webApp/pkg/models"
 
+	"github.com/go-playground/validator/v10"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm/clause"
 )
 
+// to validate a admin
+func ValidateAdmin(form struct {
+	Email    string `validate:"required,email"`
+	Password string `validate:"required"`
+}) (interface{}, bool) {
+
+	//validate given values
+
+	//crate obj of vlaidatior
+	validate := validator.New()
+
+	if err := validate.Struct(form); err != nil { //error in
+
+		var errorMessge = map[string]bool{}
+
+		for _, er := range err.(validator.ValidationErrors) {
+
+			errorMessge[er.Field()] = true
+		}
+
+		//return the error map and returrn false
+		fmt.Println("error on validation of login form")
+		return errorMessge, false
+	}
+
+	//check the admin in database
+	var admin models.Admin
+
+	db.DB.First(&admin, "email = ?", form.Email)
+
+	if admin.ID == 0 { //user not found
+		fmt.Println("admin not found in db")
+		return map[string]bool{"Email": true}, false
+	}
+
+	//hash the password and check the password
+
+	if err := bcrypt.CompareHashAndPassword([]byte(admin.Password), []byte(form.Password)); err != nil {
+		fmt.Println("password no match")
+		return map[string]bool{"Password": true}, false
+	}
+
+	//valid admin
+	fmt.Println("valid admin login")
+	return admin, true
+}
 func CreateAdmin() {
 
 	aEmail := os.Getenv("ADMINEMAIL")
